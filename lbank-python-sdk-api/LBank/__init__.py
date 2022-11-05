cbfee1dd45cc1958815d16509e089432107d0b0e
Kim daesik <eotlr0806@naver.com>
2022-11-05 20:40:53 +0900

    Delete lbank-python-sdk-api directory

--- lbank-python-sdk-api/LBank/__init__.py
+++
@@ -1,3 +0,0 @@
-# -*- coding:utf-8 -*-
-# project name: project
-# file name : __init__.py
--- lbank-python-sdk-api/LBank/new_v2_inter/__init__.py
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-# @Time    : 2022/3/8 17:18
-# @Author  : maochong
-# @File    : __init__.py.py
